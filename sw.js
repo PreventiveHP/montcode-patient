3484f02becbf07ef0565b03ca9e61e07a7cba520
PreventiveHP <mmontalvanfnp25@gmail.com>
2026-04-26 19:30:47 -0400

    Update service worker asset caching and cleanup

--- sw.js
+++ sw.js
@@ -3,26 +3,15 @@ const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './logo_192.png',
-  './logo_512.png'
+  './logo.png.jpg'  // Nombre exacto según tu GitHub
 ];
 
-// Instalación: Crea el caché con el nombre de la unidad
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[MontCode™ SW] Caching tactical assets');
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Activación: Limpia cachés antiguos
-self.addEventListener('activate', (e) => {
-  console.log('[MontCode™ SW] System Active');
-});
-
-// Fetch: Permite que la app cargue rápido
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
